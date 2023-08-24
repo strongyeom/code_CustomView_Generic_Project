33f765c06f6815eb9acfe560ca74a51ac9d344b1
yeomsungpil <present7256@gmail.com>
2023-08-24 23:07:57 +0900

    Initial Commit

---
+++ code_CustomView_Generic_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  code_CustomView_Generic_Project
+//
+//  Created by 염성필 on 2023/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
